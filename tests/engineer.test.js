3748c510a67fedf5c772ff26f152cdc022adbf8a
E73707 <94885681+E73707@users.noreply.github.com>
2023-02-02 20:21:38 +1100

    modified and finished test scipts and made some light modifications to the employee cards

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -21,12 +21,29 @@ describe("Engineer", () => {
       const obj = new Engineer();
       expect(obj.hasOwnProperty("github"));
     });
-
-    describe("get role", () => {
-      it('should return "engineer" when the "getRole" function is called', () => {
-        const obj = new Engineer();
-        expect(obj.getRole()).toEqual("Engineer");
-      });
+  });
+  describe("get role", () => {
+    it('should return "engineer" when the "getRole" function is called', () => {
+      const obj = new Engineer();
+      expect(obj.getRole()).toEqual("Engineer");
+    });
+  });
+  describe("Functions", () => {
+    it("should return the correct name when the getName function is called", () => {
+      const obj = new Engineer("ed", 34, "eddivaughan@gmail.com", "e73707");
+      expect(obj.getName()).toEqual("ed");
+    });
+    it("should return the correct id when the getId function is called", () => {
+      const obj = new Engineer("ed", 34, "eddivaughan@gmail.com", "e73707");
+      expect(obj.getId()).toEqual(34);
+    });
+    it("should return the correct email when the getEmail function is called", () => {
+      const obj = new Engineer("ed", 34, "eddivaughan@gmail.com", "e73707");
+      expect(obj.getEmail()).toEqual("eddivaughan@gmail.com");
+    });
+    it("should return the correct office nummber when the getOfficeNumber function is called", () => {
+      const obj = new Engineer("ed", 34, "eddivaughan@gmail.com", "e73707");
+      expect(obj.getGithub()).toEqual("e73707");
     });
   });
 });
--- tests/intern.test.js
+++ tests/intern.test.js
@@ -21,12 +21,29 @@ describe("Intern", () => {
       const obj = new Intern();
       expect(obj.hasOwnProperty("school"));
     });
-
-    describe("get role", () => {
-      it('should return "intern" when the "getRole" function is called', () => {
-        const obj = new Intern();
-        expect(obj.getRole()).toEqual("intern");
-      });
+  });
+  describe("get role", () => {
+    it('should return "intern" when the "getRole" function is called', () => {
+      const obj = new Intern();
+      expect(obj.getRole()).toEqual("intern");
+    });
+  });
+  describe("Functions", () => {
+    it("should return the correct name when the getName function is called", () => {
+      const obj = new Intern("ed", 34, "eddivaughan@gmail.com", "USYD");
+      expect(obj.getName()).toEqual("ed");
+    });
+    it("should return the correct id when the getId function is called", () => {
+      const obj = new Intern("ed", 34, "eddivaughan@gmail.com", "USYD");
+      expect(obj.getId()).toEqual(34);
+    });
+    it("should return the correct email when the getEmail function is called", () => {
+      const obj = new Intern("ed", 34, "eddivaughan@gmail.com", "USYD");
+      expect(obj.getEmail()).toEqual("eddivaughan@gmail.com");
+    });
+    it("should return the correct office nummber when the getSchool function is called", () => {
+      const obj = new Intern("ed", 34, "eddivaughan@gmail.com", "USYD");
+      expect(obj.getSchool()).toEqual("USYD");
     });
   });
 });
